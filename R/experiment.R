8312c83605f8c14a208fe53449a0a1bf56f54552
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-10-06 21:07:02 +0800

    feat: add `filter_samples` and `filter_variables` functions

--- R/experiment.R
+++ R/experiment.R
@@ -245,6 +245,56 @@ get_var_info <- function(exp) {
 }
 
 
+#' Filter samples based on conditions
+#'
+#' @description
+#' Filter samples based on conditions specified in the [dplyr::filter()] function.
+#' For example, `filter_samples(exp, group == "A")` will keep samples in `exp`
+#' with "group" equal to "A".
+#'
+#' @details
+#' Instead of `Experiment$filter_samples()`, which updates the [Experiment] object in place,
+#' this function returns a new `Experiment` object with samples filtered.
+#' The original `Experiment` object will not be altered.
+#' Besides, as the first argument is the `Experiment` object,
+#' this function could be used in a `magrittr` pipe.
+#'
+#' @param exp The [Experiment] object.
+#' @param ... Conditions for filtering samples, passed to [dplyr::filter()].
+#'
+#' @return The [Experiment] object.
+#' @export
+filter_samples <- function(exp, ...) {
+  new_exp <- exp$clone()
+  new_exp$filter_samples(...)
+}
+
+
+#' Filter variables based on conditions
+#'
+#' @description
+#' Filter variables based on conditions specified in the [dplyr::filter()] function.
+#' For example, `filter_variables(exp, type == "B")` will keep variables in `exp`
+#' with "type" equal to "B".
+#'
+#' @details
+#' Instead of `Experiment$filter_variables()`, which updates the [Experiment] object in place,
+#' this function returns a new `Experiment` object with variables filtered.
+#' The original `Experiment` object will not be altered.
+#' Besides, as the first argument is the `Experiment` object,
+#' this function could be used in a `magrittr` pipe.
+#'
+#' @param exp The [Experiment] object.
+#' @param ... Conditions for filtering variables, passed to [dplyr::filter()].
+#'
+#' @return The [Experiment] object.
+#' @export
+filter_variables <- function(exp, ...) {
+  new_exp <- exp$clone()
+  new_exp$filter_variables(...)
+}
+
+
 prepare_info <- function(data, rownames, col_name) {
   if (rownames) {
     return(tibble::as_tibble(tibble::rownames_to_column(data, var = col_name)))
