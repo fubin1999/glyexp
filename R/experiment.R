b91cdf05fe906ff04dc9975f098c3604172822b9
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-10-06 16:14:53 +0800

    feat: add functions fetching data from `Experiment` objects

--- R/experiment.R
+++ R/experiment.R
@@ -103,6 +103,69 @@ create_experiment <- function(name, expr_mat, sample_info, var_info, rownames = 
 }
 
 
+#' Get the expression matrix
+#'
+#' @description
+#' Get the expression matrix from an [Experiment] object.
+#'
+#' @details
+#' A copy, instead of the original matrix, will be returned.
+#' This prevents the original matrix from being altered.
+#' Alteration of data in an [Experiment] object is strictly managed
+#' by the class, so that the expression matrix, sample information,
+#' and variable information are always consistent.
+#'
+#' @param exp The [Experiment] object.
+#'
+#' @return The expression matrix.
+#' @export
+get_expr_mat <- function(exp) {
+  exp$get_expr_mat()
+}
+
+
+#' Get the sample information
+#'
+#' @description
+#' Get the sample information from an [Experiment] object.
+#'
+#' @details
+#' A copy, instead of the original tibble, will be returned.
+#' This prevents the original tibble from being altered.
+#' Alteration of data in an [Experiment] object is strictly managed
+#' by the class, so that the expression matrix, sample information,
+#' and variable information are always consistent.
+#'
+#' @param exp The [Experiment] object.
+#'
+#' @return The sample information tibble.
+#' @export
+get_sample_info <- function(exp) {
+  exp$get_sample_info()
+}
+
+
+#' Get the variable information
+#'
+#' @description
+#' Get the variable information from an [Experiment] object.
+#'
+#' @details
+#' A copy, instead of the original tibble, will be returned.
+#' This prevents the original tibble from being altered.
+#' Alteration of data in an [Experiment] object is strictly managed
+#' by the class, so that the expression matrix, sample information,
+#' and variable information are always consistent.
+#'
+#' @param exp The [Experiment] object.
+#'
+#' @return The variable information tibble.
+#' @export
+get_var_info <- function(exp) {
+  exp$get_var_info()
+}
+
+
 prepare_info <- function(data, rownames, col_name) {
   if (rownames) {
     return(tibble::as_tibble(tibble::rownames_to_column(data, var = col_name)))
--- tests/testthat/test-experiment.R
+++ tests/testthat/test-experiment.R
@@ -22,6 +22,21 @@ create_var_info <- function(variables) {
   )
 }
 
+create_test_exp <- function(samples, variables) {
+  expr_mat <- create_expr_mat(samples, variables)
+  sample_info <- create_sample_info(samples)
+  var_info <- create_var_info(variables)
+  suppressMessages(
+    exp <- create_experiment(
+      name = "my_experiment",
+      expr_mat = expr_mat,
+      sample_info = sample_info,
+      var_info = var_info
+    )
+  )
+  exp
+}
+
 # Real tests-----
 test_that("creating a new object", {
   samples <- c("S1", "S2", "S3")
@@ -228,4 +243,23 @@ test_that("getting data copies instead of original data", {
   expect_equal(exp$get_sample_info()$sample, c("S1", "S2", "S3"))
   expect_equal(exp$get_var_info()$variable, c("V1", "V2", "V3"))
   expect_equal(exp$get_expr_mat()[1, 1], 1)
+})
+
+
+test_that("getter functions work", {
+  samples <- c("S1", "S2", "S3")
+  variables <- c("V1", "V2", "V3")
+  expr_mat <- create_expr_mat(samples, variables)
+  sample_info <- create_sample_info(samples)
+  var_info <- create_var_info(variables)
+  exp <- Experiment$new(
+    name = "my_experiment",
+    expr_mat = expr_mat,
+    sample_info = sample_info,
+    var_info = var_info
+  )
+
+  expect_equal(get_expr_mat(exp), expr_mat)
+  expect_equal(get_sample_info(exp), sample_info)
+  expect_equal(get_var_info(exp), var_info)
 })
